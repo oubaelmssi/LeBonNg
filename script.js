bb7dca7744d474384fc7a8ebc413d79484b9540a
oubaelmssi <oubaelmssi@gmail.com>
2024-06-03 21:34:13 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.auction-item button').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('Faire une offre a été cliqué!');
+    });
+});
